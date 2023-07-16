31f6605d6892ef56adada45b9f88db23ac6cd781
Pavel Klochkov <paul.klochkov@gmail.com>
2023-07-16 10:58:09 +0200

    Fix problems related to update to prettier v3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   extends: ['plugin:prettier/recommended'],
   plugins: ['import', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
   env: {
     browser: true,
     es6: true,
     jquery: true,
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
     'no-duplicate-imports': 'error',
+    'prettier/prettier': 'error',
   },
 };
